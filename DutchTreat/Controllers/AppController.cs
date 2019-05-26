933aaccb0945e1ca2f0696e03995566a84b4d882
cullen-kennedy <cullen.kennedy92@gmail.com>
2019-05-26 16:31:00 -0400

    Basic MVC

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,54 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+
+        private readonly IMailService _mailService;
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //app/contact -> /contact
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+ 
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //do stuff
+                _mailService.SendMessage("cullen@gmail.com", model.Subject, model.Message);
+                ViewBag.UserMessage = "mail sent";
+            }
+            else
+            {
+                //errors
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+    var x = 0;
+    var s = "";
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying item")
+    });
+
+    //Setting up events on a collection of items
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log($(this).text())
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form")
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+});
